23a0b298aa8305b466487a7a5925d4e25c0fcecd
kinoodaphne <daphne.kinoo@outlook.be>
2019-08-08 18:40:44 +0200

    Added functions to gulp
    Bug: always have to restart gulp for sass changes to be seen

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+const {
+    src,
+    dest,
+    watch,
+    parallel
+} = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+const nodemon = require('gulp-nodemon');
+
+function sass2css(){
+    return src("./public/stylesheets/scss/style.scss")
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(dest("./public/dist/css/"))
+        .pipe(reload({stream:true}));
+}
+
+function serve() {
+  return nodemon({
+    script: './bin/www',
+  })
+  .on('restart', function(){
+    console.log('restarted');
+  })
+}
+
+function useNodemon() {
+    return nodemon({
+        script: './bin/www',
+        ext: "pug js",
+        env: { 'NODE_ENV': 'development' }
+    });
+}
+
+function doBrowserSync() {
+    return browserSync.init(null, {
+        proxy: 'http://localhost:3000',
+        port: 3000
+    });
+}
+
+function watchSass() {
+    return watch("./public/stylesheets/sass/*.scss", parallel(sass2css));
+}
+
+module.exports.default = parallel(sass2css, watchSass);
