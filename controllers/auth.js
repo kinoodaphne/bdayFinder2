6a872b7258d89a295be4e5b275490ea007a8d2ca
kinoodaphne <daphne.kinoo@outlook.be>
2019-08-09 15:42:58 +0200

    Created javascript-file signup -> 404 Not Found

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,11 +3,15 @@ const User = require('../models/User');
 const signup = async (req, res, next) => {
     let username = req.body.username;
     let password = req.body.password;
-    const user = new User({username: username});
+
+    const user = new User({username: username, password: password});
     await user.setPassword(password);
     await user.save().then(result => {
         res.json({
-            "status": "success"
+            "status": "success",
+            "data": {
+                "user": result
+            }
         })
     }).catch(error => {
         res.json({
---
+++ public/javascripts/signup.js
@@ -0,0 +1,32 @@
+var btnSignup = document.querySelector(".signup button").addEventListener("click", (e) => {
+    let username = document.querySelector('#email').value;
+    let password = document.querySelector('#password').value;
+
+    fetch('http://localhost:3000/users/signup', {
+        method: 'post',
+        header: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "username": username,
+            "password": password
+        })
+    }).then(response => {
+        return response.json();
+    }).then(json => {
+        if (json.status === "success") {
+            let feedback = document.querySelector(".feedback");
+            feedback.textContent = "Sign up complete";
+            feedback.clasList.remove('hidden');
+
+            let token = json.data.token;
+            let user_id = json.data.user_id;
+            let username = json.data.username;
+            
+            localStorage.setItem('token', token);
+            localStorage.setItem("user_id", user_id);
+            localStorage.setItem("username", username);
+            window.location.href = "http://localhost:3000";
+        }
+    }  )
+});
