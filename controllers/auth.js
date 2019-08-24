dc63d0989ab8493b2f8ecab64c25cad591e84883
kinoodaphne <daphne.kinoo@outlook.be>
2019-08-24 22:15:45 +0200

    Dropped indexes in db.user
    Bug: User get registered twice in database now.
    
    example: /* 25 */
    {
        "_id" : ObjectId("5d619a74aa1efc050879e3ca"),
        "username" : "hannahbaker",
        "salt" : "ac12f4df2e790bd6df24fa864065959e43e59bd61fe7286d4c780a070049b5c0",
        "hash" : "ef9f953bdf1426621ce7ac521f56f650933dba706b71ef310de8bbffb96e6b1194ddd160b515b1c75b8172fc899b294d2ba44c7181b07f05bab1ba757f3ff939eddedda77ff9ca25b1bd7b7afb4955e0fb1ceb9b3807caceb876edd4ff17aaccde06e554430d44f989f63a43f02cc041ce244185964ed4404374ea7e17b6386b1107e23ed9a4faceac4bdcde71cdaa63e463800858dee5162f391bd57c3084266c228d1b7c2a0a277bcc106d5f1a891eae6d6c74bb6396853853946c3de6767650a4f6723ba6fb48f3a03bf0d4b1d45781efd5de7f3c51b4e4e32edf49bf9eaf05c154140588ae5a53860666620afb452e08f2a40e3bdee22a6740cbf900b143c88269a080354251ad25a5a30fce7c84318a6ec9019b183fd50bff6f83e885cd0532f35d94b86abdac6849546e9c1536d561aafe8f360cce0395d6cec13d611f58a678e31ce76f0cbd178653c2c2a1aa4f3c6c82dd6406e16a34441b87b20d0b34dd20d8f229c336e21ddd5b797d5745e45d3c23de36843e823e51583457ce369503229a0cd9eec46707221c3b069e3dc00ac7faff6a4cfa3b394d304423c569dbfa4e1af30a81554e130e0543bbf6e8af34e4933eec2edbebc60d3483e2a05276912538220563daeef4149f53817884f28c9358109430aa79ad8e4adf3e692ebdbd0f70f78f193fad2e96120d549899ef68df1bccb188a59486ac10efded9d9",
        "__v" : 0
    }
    
    /* 26 */
    {
        "_id" : ObjectId("5d619a74aa1efc050879e3cb"),
        "salt" : "5e81d675e84cf317a41fd915d9093f22ed5f18d73cf3ea3c88e3cd59495a3ca9",
        "hash" : "e18482261839842f4abb76691a31eaa0ce1640560b374c5fc0d07d31e08dd4673cdb3bdd5c9261d7026b98d109d217e265f10289198f923f3254b1250fc1870a305492d79b58467f3b50293a816c1d2b0ee2120fae7bc819f0e8be8690aed5ab5d59dac032ac15f98b8a3c07b2b0a7cb35b1a4720e0a5919ae20d6e6f4eda7f50c827ed0da5ff24715a1f2ad240fe37359f7a0ca3eaa8bac241c4bc4c9fffe59d2ea5bb817447ee48e6b1481aca5556161ad2acae008f3a70e42635888c5c7cf873ca205704a5644a4419a32ab74d6e4e5a94f64dc51e81614c6544f57e239ad101cb04abfd07f914fc76091af43e7a3e15eb7eeba29acb3ec6657ccee33459690e9994b5976f55eecd9a41b3a45146d4e227390fb67e46834208071044789a5b6ab45987572118b7b43675a40156706863b2ecb5a9ec7f5d70771230fd00b8a4384d5d22d54354242f773cbfc62e1bb9e8bf8f39a6f16b953c749f02da7b09a0cd2304a025e07c69ca1d6e739d7e6031a1e0bc2ae2f595d340a39dd0a26b97488e955bb7d85c5bb77c85994973623f6a3b37b53636d853c2295b1d945a5dc875442471c2891025ca50a855c30a7a7084fb58676b129ca5563441f3913f34bca1b6ad55ade4017643348397a4363236e736359ed12a7cbf3ad6fc15438c157acde6558b10d45147d1aafea483f884bf5c82381d1464ae098ab6122b3511ec52a",
        "__v" : 0
    }

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,10 +6,12 @@ const signup = async (req, res, next) => {
     let username = req.body.username;
     let password = req.body.password;
 
-    const user = new User({username: username});
+    const user = new User({
+        username: username
+    });
     await user.setPassword(password);
     await user.save().then(result => {
-         let token = jwt.sign({
+        let token = jwt.sign({
             uid: result._id,
             username: result.username
         }, Config.get('jwt.secret'));
